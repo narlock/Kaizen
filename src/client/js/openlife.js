86b7ba969ad79efd29af1873f73e4b3b31c18c2c
narlock <anthonynarlock@outlook.com>
2022-08-06 17:26:59 -0500

    OpenLife v0.1 : CSS Additions

--- src/client/js/openlife.js
+++ src/client/js/openlife.js
@@ -20,22 +20,45 @@ if (!event.target.matches('.dropbtn')) {
 
 document.getElementById("incrementCounter").addEventListener("click", addIncrementDecrementCounterWidget);
 function addIncrementDecrementCounterWidget() {
+    //Div
     const widgetDiv = document.createElement("div");
+    widgetDiv.setAttribute("id", "container");
+
     widgetDiv.style.borderRadius = "25px";
-    widgetDiv.style.background = "yellow";
+    widgetDiv.style.border = "5px solid yellow";
     widgetDiv.style.padding = "20px";
     widgetDiv.style.width = "200px";
     widgetDiv.style.height = "150px";
 
+    //Title Element
+    var titleHeader = document.createElement("p");
+        titleHeader.innerText = "Counter";
+        titleHeader.style.fontSize = "small";
+
+    //Counter Element
     var counterParagraph = document.createElement("span");
+        counterParagraph.style.fontSize = "x-large";
     const counterButton = document.createElement("button");
         counterButton.innerText = "+";
+        counterButton.style.fontSize = "x-large";
+
+    //Description Element
+    var descParagraph = document.createElement("p");
+        descParagraph.innerHTML = "This is the increment widget. Use the button to increment the counter.";
+        descParagraph.style.fontSize = "small";
+
+    //Widget Setup
+    widgetDiv.appendChild(titleHeader);
     widgetDiv.appendChild(counterParagraph);
     widgetDiv.appendChild(counterButton);
-    document.body.appendChild(widgetDiv);
+    widgetDiv.appendChild(descParagraph);
 
+    gridBox = document.querySelector(".grid-item");
+    // gridBox.removeAttribute("id");
+    gridBox.appendChild(widgetDiv);
+
+    //Counter functionality
     var count = 0;
-    
     counterButton.addEventListener("click", function() {
         console.log(count);
         console.log(counterParagraph);
