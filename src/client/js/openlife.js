3a15aa7f97300d7e192a72484c7990590d9b6433
narlock <anthonynarlock@outlook.com>
2022-08-06 18:21:29 -0500

    OpenLife v0.1 : CSS updates, clock

--- src/client/js/openlife.js
+++ src/client/js/openlife.js
@@ -18,33 +18,54 @@ if (!event.target.matches('.dropbtn')) {
 }
 }
 
+//Clock
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
+//IncrementCounter
 document.getElementById("incrementCounter").addEventListener("click", addIncrementDecrementCounterWidget);
 function addIncrementDecrementCounterWidget() {
     //Div
     const widgetDiv = document.createElement("div");
     widgetDiv.setAttribute("id", "container");
 
+    widgetDiv.style.background = "white";
     widgetDiv.style.borderRadius = "25px";
-    widgetDiv.style.border = "5px solid yellow";
+    widgetDiv.style.border = "5px solid blue";
     widgetDiv.style.padding = "20px";
     widgetDiv.style.width = "200px";
     widgetDiv.style.height = "150px";
 
     //Title Element
     var titleHeader = document.createElement("p");
-        titleHeader.innerText = "Counter";
-        titleHeader.style.fontSize = "small";
+        titleHeader.innerHTML = "<b>Counter</b>";
+        titleHeader.style.fontSize = "medium";
 
     //Counter Element
     var counterParagraph = document.createElement("span");
+        counterParagraph.innerHTML = "0";
         counterParagraph.style.fontSize = "x-large";
+        counterParagraph.style.paddingRight = "20px";
     const counterButton = document.createElement("button");
         counterButton.innerText = "+";
         counterButton.style.fontSize = "x-large";
 
     //Description Element
     var descParagraph = document.createElement("p");
-        descParagraph.innerHTML = "This is the increment widget. Use the button to increment the counter.";
+        descParagraph.innerHTML = "Use the button to increment the counter.";
         descParagraph.style.fontSize = "small";
 
     //Widget Setup
@@ -54,7 +75,14 @@ function addIncrementDecrementCounterWidget() {
     widgetDiv.appendChild(descParagraph);
 
     gridBox = document.querySelector(".grid-item");
-    // gridBox.removeAttribute("id");
+    gridBox.removeAttribute("class");
+    //Removing this attribute allows the next grid-item to be used
+
+    gridBox.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+    gridBox.style.border = "1px solid rgba(0,0,0,0.8)";
+    gridBox.style.padding = "20px";
+    gridBox.style.textAlign = "center";
+
     gridBox.appendChild(widgetDiv);
 
     //Counter functionality
